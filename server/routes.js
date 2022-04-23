522519e5ebb9483d59f09d90f54fa9fe3b341f89
Andrew Tran <amtran150@gmail.com>
2022-04-22 19:48:07 -0700

    created features route

--- server/routes.js
+++ server/routes.js
@@ -19,4 +19,52 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:product_id', (req, res) => {
+  let id = req.params.product_id;
+  query()
+    .then(client => {
+      client
+        // .query(`
+        //   SELECT json_build_object(
+        //     'id', products.id,
+        //     'name', products.name,
+        //     'slogan', products.slogan,
+        //     'description', products.description,
+        //     'category', products.category,
+        //     'default_price', products.default_price,
+        //     'features', (json_build_array(json_build_object(
+        //       'feature', features.feature,
+        //       'value', features.value
+        //     ))))
+        //   FROM products
+        //   LEFT JOIN features ON features.product_id = products.id
+        //   WHERE products.id = ${id}
+        // `)
+        .query(`
+          SELECT
+            products.id AS id,
+            products.name AS name,
+            products.slogan AS slogan,
+            products.description AS description,
+            products.category AS category,
+            products.default_price AS default_price,
+            (array_agg(
+              json_build_object(
+              'feature', features.feature,
+              'value', features.value
+              )
+            )) AS features
+          FROM products
+          LEFT JOIN features ON features.product_id = products.id
+          WHERE products.id = ${id}
+          GROUP BY products.id
+        `)
+        .then((result) => {
+          client.release();
+          console.log(result.rows);
+          res.send(result.rows[0])
+        })
+    })
+})
+
 module.exports = router;
