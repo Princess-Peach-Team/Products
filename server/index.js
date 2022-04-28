e2096f382ee735ebf3a0b19a5df9b4001788a344
Andrew Tran <amtran150@gmail.com>
2022-04-28 11:36:47 -0700

    Loader token added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
+app.get('/loaderio-b6aa0914f0ad1eae276fdd4417f74098.txt', (req, res) => {
+  res.status(200).send(process.env.LOADER_TOKEN);
+})
 app.use('/products', routes);
 
 
